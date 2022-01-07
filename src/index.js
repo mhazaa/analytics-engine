b277f5c49314eedffa69cecbfed873b33abd6326
mhazaa <magdihazaa@gmail.com>
2022-01-07 00:52:13 -0600

    switching into esm instead of cjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AnalyticsEngineClient = exports.AnalyticsEngine = void 0;
-const AnalyticsEngine_1 = __importDefault(require("../lib/AnalyticsEngine"));
-exports.AnalyticsEngine = AnalyticsEngine_1.default;
-const AnalyticsEngineClient_1 = __importDefault(require("../lib/AnalyticsEngineClient"));
-exports.AnalyticsEngineClient = AnalyticsEngineClient_1.default;
+import AnalyticsEngine from '../lib/AnalyticsEngine';
+import AnalyticsEngineClient from '../lib/AnalyticsEngineClient';
+export { AnalyticsEngine, AnalyticsEngineClient };
